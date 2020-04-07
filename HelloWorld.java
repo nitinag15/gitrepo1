1815e518c68585d027aa3c2a40de4c63ce2c4d9c
nitinag15 <63226550+nitinag15@users.noreply.github.com>
2020-04-07 16:54:40 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+?**
+* The Hello World App lass implements an application that simply prints "Hello World"
+*/
+public class HelloWorld {
+	public static void main(String[] args){
+		system.out.println("Hello World");
+	}
+}
